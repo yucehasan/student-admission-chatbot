38c4298d13750370effc558b5c9893b137b0355f
yucehasan <yuce.kilic@technarts.com>
2025-04-03 20:26:11 -0400

    Update backend

--- backend/app/endpoints.py
+++ backend/app/endpoints.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from datetime import datetime
 import json
+import requests
 
 
 from .socket import ConnectionManager
+
+API_KEY = ""
 
 router = APIRouter()
 manager = ConnectionManager()
@@ -13,7 +16,27 @@ def respond_to_message(message):
     if message == "Connect":
         response = "Hello, how can I help you?"
     else:
-        response = "I don't know yet"
+        # API endpoint
+        url = "https://pdf.ai/api/v1/chat-all"
+        # Headers
+        headers = {
+            "X-API-Key": API_KEY,
+            "Content-Type": "application/json"
+        }
+        # Request body
+        data = {
+            "save_chat": False,
+            "message": message,  # Your question
+            "use_gpt4": False,  # Set to True to use GPT-4 model
+            "model": "gpt-3.5-turbo",  # Choose between "gpt-4o" or "gpt-4-turbo"
+            "language": "english",  # Change to preferred language
+        }
+
+        # Make the POST request
+        res = requests.post(url, json=data, headers=headers)
+        response = dict(res.json()).get("content", "No response")
+        
+        
     return response
 
 @router.websocket("/ws/{client_id}")
